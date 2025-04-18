fa35a62ef87aaa2dea7b7ec4ca5ba32d74c7808a
Thiago Corrêa <99252640+thiagocorreanet@users.noreply.github.com>
2025-04-18 19:53:50 -0300

    Add files via upload

---
+++ workers/WorkerService/WorkerService/Worker/ProcessamentoPedidosWorker.cs
@@ -0,0 +1,44 @@
+﻿namespace WorkerService.Worker;
+
+public class ProcessamentoPedidosWorker : BackgroundService
+{
+    private readonly ILogger<ProcessamentoPedidosWorker> _logger;
+
+    public ProcessamentoPedidosWorker(ILogger<ProcessamentoPedidosWorker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("ProcessamentoPedidosWorker starting.");
+            await Task.Delay(5000, stoppingToken);
+        }
+        
+    }
+}
+
+public class ProcessamentoPedidosWorkerV2 : BackgroundService
+{
+    private readonly ILogger<ProcessamentoPedidosWorker> _logger;
+
+    public ProcessamentoPedidosWorkerV2(ILogger<ProcessamentoPedidosWorker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        TimeSpan intervalo = TimeSpan.FromSeconds(10);
+        using PeriodicTimer timer = new(intervalo);
+
+        while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            _logger.LogInformation($"Processamento de pedidos - {DateTime.Now.TimeOfDay.ToString()}");
+            await Task.Delay(5000, stoppingToken);
+        }
+
+    }
+}
